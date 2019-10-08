db0e322701b8f613ea533b8fed8fa6bd16c82be1
CScori <amilanotte1366@gmail.com>
2019-10-08 14:07:44 -0500

    adds reducer and initial state

---
+++ reducer/src/components/Todo.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+const todo = () => {
+    
+    return (
+        <div>
+            
+  item: 'Learn about reducers',
+  completed: false,
+  id: 3892987589
+        </div>
+    )
+}
+
+export default todo
